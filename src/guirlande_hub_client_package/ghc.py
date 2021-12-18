c248068e9eb7c71d42ba5f2b118bbf425fdb3078
Lelberto <jsurieux@gmail.com>
2021-12-18 20:59:02 +0100

    Add module class with send method WIP

--- src/guirlande_hub_client_package/ghc.py
+++ src/guirlande_hub_client_package/ghc.py
@@ -1,2 +1,16 @@
 import requests
 import socketio
+
+class Module:
+  """Module class used to instantiate the module and communicate with the backend"""
+
+  def __init__(self, type):
+    self.type = type
+    self.__token = ''
+    self.socket = socketio.Client()
+    self.listeners = []
+  
+  def send(self, eventName, data):
+    eventName = 'module.' + self.type + '.' + eventName
+    self.socket.emit(eventName, data)
+    print('Sended event %s', eventName)
